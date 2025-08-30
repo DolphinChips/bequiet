bbb041928219a8c5a2633438026161c07369ec50
Dolphin von Chips <td2505052@gmail.com>
2025-08-30 09:54:54 +0500

    core.bequiet.services.Ratings: return Float in forChartAndLamp

--- core/src/main/scala/bequiet/services/Ratings.scala
+++ core/src/main/scala/bequiet/services/Ratings.scala
@@ -4,14 +4,14 @@ import cats.effect.*
 import cats.syntax.all.*
 import bequiet.domain.score.Lamp
 import bequiet.domain.chart.ChartId
-import bequiet.domain.rating.Rating
+// import bequiet.domain.rating.Rating
 import doobie.*
 import doobie.implicits.*
 import doobie.util.*
 
 trait Ratings[F[_]]:
   def create(chartId: ChartId, lamp: Lamp, rating: Float): F[Unit]
-  def forChartAndLamp(chartId: ChartId, lamp: Lamp): F[Rating]
+  def forChartAndLamp(chartId: ChartId, lamp: Lamp): F[Float]
 
 object Ratings:
   def apply[F[_]](using ratings: Ratings[F]) = ratings
@@ -35,8 +35,8 @@ final case class LiveRatings[F[_]: Concurrent](private val xa: Transactor[F])
   def forChartAndLamp(chartId: ChartId, lamp: Lamp) =
     sql"""
       select value from rating
-      where chart_id = $chartId and lamp = $lamp
+      where chart_id = $chartId and lamp_id = $lamp
     """
-      .query[Rating]
+      .query[Float]
       .unique
       .transact(xa)
